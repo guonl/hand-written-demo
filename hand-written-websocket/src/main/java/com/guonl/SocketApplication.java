e8504ec14696e034c20063c049709cb800758b78
guonl <983220871@qq.com>
2021-01-28 18:51:14 +0800

    添加定时器

--- hand-written-websocket/src/main/java/com/guonl/SocketApplication.java
+++ hand-written-websocket/src/main/java/com/guonl/SocketApplication.java
@@ -2,10 +2,12 @@ package com.guonl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * 参考文章：https://www.cnblogs.com/xuwenjin/p/12664650.html
  */
+@EnableScheduling
 @SpringBootApplication
 public class SocketApplication {
 
